8b534c9f0f7093aa449b55e18b9572949e3693b7
Dmitrii Barilkin <dimabaril@gmail.com>
2024-05-14 20:15:51 +0200

    fix: kill comments

--- src/pages/main/api/index.ts
+++ src/pages/main/api/index.ts
@@ -6,21 +6,3 @@ const fetchProductProjections = () =>
   apiRoot.productProjections().get().execute();
 
 export { fetchDiscountCodes, fetchProductProjections };
-
-// fetchProductProjections().then(({ body: { count, results } }) => {
-//   for (let i = 0; i < 3; i += 1) {
-//     const randomProduct = results[Math.floor(Math.random() * count)];
-//     console.log(randomProduct);
-//   }
-// });
-
-// apiRoot
-//   .productProjections()
-//   .search()
-//   .get({
-//     queryArgs: {
-//       limit: 2,
-//       offset: 2,
-//     },
-//   })
-//   .execute();
--- src/pages/main/ui/mainPageView.ts
+++ src/pages/main/ui/mainPageView.ts
@@ -3,22 +3,9 @@ import CreateElement from "../../../shared/helpers/element-create";
 import cleanContainer from "../../../shared/utils/clean-container";
 
 export default class MainPageView {
-  // private title = new CreateElement({
-  //   tag: "h1",
-  //   cssClasses: ["main-page__title"],
-  //   textContent: "Main page",
-  // });
-
-  // private content = new CreateElement({
-  //   tag: "div",
-  //   cssClasses: ["main-page__content"],
-  //   textContent: "Maybe some text on main page.",
-  // });
-
   private container = new CreateElement({
     tag: "main",
     cssClasses: ["main-page"],
-    // children: [this.content],
   });
 
   appendContent(...contents: HTMLElement[]) {
