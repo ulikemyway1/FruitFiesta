c0b4135ac2d4ccd0651c2463e8125d4998b3b013
Dmitrii Barilkin <dimabaril@gmail.com>
2024-06-16 03:20:59 +0200

    fix

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -37,8 +37,8 @@ class User {
   set userIsLoggedIn(value: boolean) {
     this.privateUserIsLoggedIn = value;
     basketModel.resetCart();
-    // this.notify();
-    basketModel.getCart().then(() => this.notify());
+    basketModel.getCart();
+    this.notify();
   }
 
   set userInfo(userData: Customer | null) {
